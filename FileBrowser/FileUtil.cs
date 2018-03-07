d5b768b1583d498d342932c3ffbde2c119930adf
scriptingbear <adiv.abramson@yahoo.com>
2018-03-06 18:07:30 -0600

    Updated with more detailed comments throughout

--- FileBrowser/FileUtil.cs
+++ FileBrowser/FileUtil.cs
@@ -9,6 +9,14 @@ namespace FileBrowser
 {
     /// <summary>
     /// A static class containing methods for handling folders and files
+    /// This way we can just call the methods directly off of the class
+    /// instead of instantiating an object and calling methods of the instance.
+    /// Only use instance methods when they actually modify properties or data
+    /// of the instance. If you're creating straight up utility methods that
+    /// don't modify the class in any way, you can make the class static and
+    /// all of its methods static. This is how the System.Console class works.
+    /// We don't have to write something like Console console = new Console();
+    /// in order to use its WriteLine() or ReadKey() methods
     /// </summary>
     static class FileUtil
     {
@@ -18,6 +26,10 @@ namespace FileBrowser
         /// <returns></returns>
         public static List<string> GetDrives()
         {
+            /*Use the DriveInfo class's GetDrives() method to return
+             * a list of DriveInfo objects (not just plain strings corresponding
+             * to drive letters on the machine) for all the drive letters
+             */ 
             List<string> drives = new List<string>();
             DriveInfo[] driveInfo = DriveInfo.GetDrives();
 
@@ -39,16 +51,33 @@ namespace FileBrowser
         /// <returns></returns>
         public static List<FileInfo> GetFilesInFolder(string folder, int SortColumn, string OrderDirection)
         {
-
+            /*The generic list of FileInfo objects declared below is just a dummy object
+             * Each case branch in the switch{} block returns an object to the calling code
+             * But if you omit the default block, C# is going to complain about the method
+             * not always having a return path. In this particular applicaition, this method,
+             * GetFilesInFolder() will **never** execute the default branch in the switch{} 
+             * block
+             */ 
             List<FileInfo> files = new List<FileInfo>();
 
             DirectoryInfo directoryInfo = new DirectoryInfo(folder);
-
+            /* Using Linq query syntax here to get a list of files in the specified folder
+             * since the sorting field will differ depending on how this method is called
+             * we can't put code to determine the sorting field inside the Linq query
+             * itself (I tried; it doesn't work!). So we defer sorting until after the list
+             * has been built.
+             */ 
             var fileList = from file in directoryInfo.EnumerateFiles()
                            where file.Name[0] != '$' && file.Name[0] != '.'
-                           //orderby file.Name
                            select file;
 
+            /* Now use method syntax in Linq to sort the file list by the selected column
+             * and in either ascending or descending order, depending on the parameters passed
+             * to the method when it is called
+             * Note the the method's return type is List<FileInfo>. The result of a Linq query, after
+             * the OrderBy() method has been called on it, is IOrderedEnumerable. So we use
+             * ToList() to cast it into type List<FileInfo> before returning to the calling code
+             */ 
             switch(SortColumn)
             {
                 case 0:
@@ -84,7 +113,12 @@ namespace FileBrowser
 
         public static List<string> GetFoldersInDrive(string drive)
         {
-            
+            /* Here we use Linq again to return a list of items, this time
+             * a list of folders in the specified drive. We use the DirectoryInfo class
+             * and call its EnumerateDirectories() method to get the list of folders,
+             * represented by a collection DirectoryInfo objects, each of which 
+             * has several pieces of information about a folder
+             */ 
             DirectoryInfo directoryInfo = new DirectoryInfo(drive);
 
             var folders = from folder in directoryInfo.EnumerateDirectories()
--- FileBrowser/frmBrowser.cs
+++ FileBrowser/frmBrowser.cs
@@ -20,21 +20,44 @@ namespace FileBrowser
 
         private void frmBrowser_Load(object sender, EventArgs e)
         {
-            //get list of drives on this machine
+            /*get list of drives on this machine and populate the 
+            DriveList listbox control; could have iterated through the
+            list of drives returned by the GetDrives() method and used
+            the ListBox.Add() method to put each drive letter (as a string)
+            into the DriveList listbox control, but I wanted to experiment
+            with setting the ListBox.DataSource property to an in memory collection
             DriveList.DataSource = FileUtil.GetDrives();
+            */
 
         }//frmBrowser_Load()
 
         private void DriveList_SelectedIndexChanged(object sender, EventArgs e)
         {
+            /*When the user selects a drive letter, populate the FolderList listbox
+             * control from an in memory collection of strings corresponding to
+             * the folder names under the selected drive letter. Again, here
+             * we experiment with setting the DataSource property of a listbox
+             * control to a collection of items, in this case an array of strings
+             * returned by the call to the custom method GetFoldersInDrive(), which
+             * is part of the FileUtil class
+             */
             DriveInfo drive = new DriveInfo(DriveList.SelectedItem.ToString());
 
+            /*the selected drive letter may not be ready, such as if it's an empty optical drive
+             * so it is important to see if the drive is "ready" before trying to read its list of
+             * folders
+             */
             if (drive.IsReady)
             {
                 FolderList.DataSource = FileUtil.GetFoldersInDrive(drive.Name);
             } else
 
             {
+                /*if the selected drive is not ready then clear out the FolderList listbox
+                 * control. Note that we can't use the ListBox.Items.Clear() method here because
+                 * once the DataSource property has been set, the Items collection of a listbox
+                 * is no longer editable
+                 */
                 FolderList.DataSource = null;
             }
 
@@ -43,17 +66,36 @@ namespace FileBrowser
 
         private void FillFileList(int SortColumn = 0, string OrderDirection = "ASC")
         {
-            //might get permissions error
+            /*might get permissions error so enclose the code to populate the listview
+             * control in a try...catch block
+             */ 
             try
             {
+                /*GetFilesInFolder() returns a list of FileInfo objects, one for each file in the
+                 * selected folder. We could have designed the function to return an array (or List)
+                 * of strings and then extract the required information such as last modified date
+                 * using the File class. But I wanted to work with FileInfo objects to practice
+                 * using their properties. As it turns out, a FileInfo object does not expose
+                 * properties such as file creation date so we end up using the File class methods like
+                 * File.GetCreationTime() anyway!
+                 */
                 List<FileInfo> files = FileUtil.GetFilesInFolder(DriveList.SelectedItem.ToString()
                                                 + FolderList.SelectedItem.ToString(), SortColumn, OrderDirection);
 
+                /*don't want to accumulate filenames between actions that cause the
+                 * FileList listview control to be populated so we must empty it out each 
+                 * time we want to insert a colleciton of ListViewItem objects
+                */
                 FileList.Items.Clear();
 
                 //need filename, created, modified, size and type
                 foreach (FileInfo file in files)
                 {
+                    /*A row in a listview control is a ListViewItem. We must create
+                     * a ListViewItem, which is an array of strings, and then pass
+                     * that to the listview control's Add() method, for each file we
+                     * get back from calling GetFilesInFolder()
+                     */
                     var listViewItem = new ListViewItem(new string[]
                         {file.Name,
                     File.GetCreationTime(file.FullName).ToString("d"),
@@ -68,6 +110,7 @@ namespace FileBrowser
             }
             catch (Exception ex)
             {
+                //in case something goes wrong, let's display a message to the user
                 FileList.Items.Clear();
                 MessageBox.Show("Can't view this folder's contents.","File Browser", 
                                 MessageBoxButtons.OK,MessageBoxIcon.Warning);
@@ -78,14 +121,31 @@ namespace FileBrowser
 
         private void FolderList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //sort by filename and ascending order by default
+            /*sort by filename and ascending order by default when the user selects
+             * a different folder; we only apply custom sorting if the user has clicked a
+             * column header on the listview control. So here, the call to FillFileList()
+             * takes no arguments and instead the default values for sorting column and 
+             * sorting order will be applieed
+             */ 
             FillFileList();
             
             }// FolderList_SelectedIndexChanged()
 
         private void FileList_ColumnClick(object sender, ColumnClickEventArgs e)
         {
-            
+            /* To sort the listview control, which contains a list of filenames and associated
+             * details like size and last date modified, we need to know which column was clicked,
+             * which the e parameter in FileList_ColumnClick() allows us to access. We also
+             * need to know whether to sort a given column in ascending or descending order.
+             * 
+             * To do that, when designing the listview's columns collection, I specified a default
+             * value of "ASC" in each column header's Tag property.
+             * 
+             * The columns List below contains objects corresponding to the column headers
+             * in the listview control. We would not be able to see these objects unless
+             * we have made them public, which you can set in the column header properties
+             * dialog when editing the listview control
+             */
 
             List<ColumnHeader> columns = new List<ColumnHeader>(new []{
 
@@ -96,10 +156,18 @@ namespace FileBrowser
                 columnHeader5
             });
 
-            //MessageBox.Show(e.Column.ToString() + ", " + columns[e.Column].Tag.ToString());
+           /*the Tag property of the column looks like a string but C# requires it to be
+            * cast (converted) to a string, so it can be passed as the 2nd argument in the call
+            * to FillFileList()
+            * Note the use of the ternary operator for more compact code
+           */
 
             FillFileList(e.Column, columns[e.Column].Tag.ToString());
-            //flip sorting order tag for column
+            /*flip sorting order tag for column, i.e. from ascending to descending
+             * and vice versa
+             * Note, as mentioned elsewhere, we must cast (convert) the column header Tag
+             * property value to a string, even though it already looks like a string
+            */
             columns[e.Column].Tag  =   ((string)columns[e.Column].Tag == "ASC") ? "DESC" : "ASC";
 
         }
